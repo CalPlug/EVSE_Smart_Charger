d61974767f505411ad38337b3aeb761315f97aeb
Luis <lecontr1@uci.edu>
2018-02-13 18:02:28 -0800

    -added new functions

--- EVSE code/NXP code/main.c
+++ EVSE code/NXP code/main.c
@@ -5,7 +5,6 @@
 #include <AppApi.h>
 
 //#define DEBUG
-
 typedef struct {
 	int pwm_high, pwm_low;
 	char state;
@@ -16,7 +15,12 @@ typedef struct {
 typedef int bool;
 enum {false, true};
 
+void InitializeCharge(ChargeState* charge);
 void LevelDetection(ChargeState* charge);
+void readPilot(ChargeState* charge);
+void setRelay(ChargeState* charge);
+void readWattmeterSPI(void);
+void save2Flash(void);
 
 int main() {
 	// must call this function to initialize the API
@@ -29,14 +33,62 @@ int main() {
 	// eAppApiPlmeSet(PHY_PIB_ATTR_TX_POWER, x);
 
 
+	// supports only up to three slaves for SPI!
+	// DO0 outputs the Clock (SPICLK), DO1 for Data
+	// (SPIMISO) and DIO18 for Data Out (SPIMOSI)
+	// Three slave select output lines can be used. SPISEL0,
+	// SPISEL1, and SPISEL2. These lines can be moved (ch 14.1)
+
+	uint8 SPINUM = 2;
+	// wattmeter and flash
+	// remember to ask what flash is... o_o - Luis
+	uint8 clockdivider = 2;
+	vAHI_SpiConfigure(SPINUM, true, false, false, 1, true, false);
+	/* # of spi slaves to control, enabled transfer w/
+	   least significant bit, clock polarity unchanged, latching
+	   data on leading edge of clock, peripheral clock is divided
+	   by 2 * clockdivider,  enabled interrupts when SPI transfer
+	   is complete, disabled automatic slave selection
+	   vAHI_SpiConfigure(SPINUM, true, false, false, 1, true, false);
+	*/
 	ChargeState charge;
+	InitializeCharge(charge);
 	LevelDetection(&charge);
 }
+void initializeGFI(void) {
 
+}
+void save2Flash(void) {
+
+}
+
+void InitializeCharge(ChargeState* charge) {
+	charge->pwm_high = 12;
+	charge->pwm_low = 12;
+
+}
+
+void readWattmeterSPI(void){
+	uint8 slave = 1;
+	// slave select works by enabling a single bit in
+	// slave variable. Each bit corresponds to a different slave
+	// apparently vAHI_SpiSelect(0) clears active slave-select lines
+	vAHI_SpiSelect(slave);
+
+	uint8 charlength = 7;
+	uint32 DataOut = 1;
+	vAHI_SpiStartTransfer(charlength, DataOut);
+	uint32 dataread = u32AHI_SpiReadTransfer32();
+	vAHI_SpiStop(); // clears any active slave-select lines
+
+
+	// this should be able to read the wattmeter through the SPI.
+	// need to convert this uint32 into an integer
+}
 void LevelDetection(ChargeState* charge) {
-	uint32_t value = u32AHI_DioReadInput();
-	uint32_t test = 10240;
-	uint32_t result = test & value;
+	uint32 value = u32AHI_DioReadInput();
+	uint32 test = 10240;
+	uint32 result = test & value;
 
 	if(result == 8192){
 			charge->lvl_1 = true;
@@ -44,26 +96,30 @@ void LevelDetection(ChargeState* charge) {
 			#ifdef DEBUG
 			printf("Level 1 charge\n");
 			#endif
-		}
-		else if(result == 0){
-			charge->lvl_1 = false;
-			charge->lvl_2 = true;
-			#ifdef DEBUG
-			printf("Level 2 charge\n");
-			#endif
-		}
-		else {
-			charge->lvl_1 = false;
-			charge->lvl_2 = false;
-			#ifdef DEBUG
-			printf("No level charge detected\n");
-			#endif
+	}
+	else if(result == 0){
+		charge->lvl_1 = false;
+		charge->lvl_2 = true;
+		#ifdef DEBUG
+		printf("Level 2 charge\n");
+		#endif
+	}
+	else {
+		charge->lvl_1 = false;
+		charge->lvl_2 = false;
+		#ifdef DEBUG
+		printf("No level charge detected\n");
+		#endif
+	}
+}
 
-		}
+void readPilot(ChargeState* charge) {
+	char state = 'A';
+	Set_State(&charge, state);
 }
 
 void setRelay(ChargeState* charge) {
-	bool DC_Relay1, DC_Relay2;
+	bool DC_Relay1, DC_Relay2 = false;
 
 	int P_H = charge->pwm_high;
 	int P_L = charge->pwm_low;
@@ -74,7 +130,6 @@ void setRelay(ChargeState* charge) {
 		/*not connected*/
 		DC_Relay1 = false;
 		DC_Relay2 = false;
-
 	}
 
 	else if ((P_H == 9 && P_L == -12) || (P_H == 6 && P_L == -12) || (P_H == 3 && P_L == -12)){
